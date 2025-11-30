39285d67970d27cfbd7d728b0f26680dfe761bd8
shakilxvs <shakilxvs@gmail.com>
2025-12-01 01:58:01 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+window.__overlayConfig = {
+  "enabledStores": [
+    "voysoul.myshopify.com",
+    "bh3f0j-es.myshopify.com"
+
+  ],
+  "overlay": {
+    "title": "Temporary Interface",
+    "subtitle": "This store is showing a temporary interface controlled from GitHub Pages.",
+    "showCloseButton": false,
+    "background": "rgba(7, 10, 15, 0.95)",
+    "color": "#ffffff"
+  }
+};
